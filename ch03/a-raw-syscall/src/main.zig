08c287ac7390b3ed21962e283b6768fde93fa862
Hiroaki Nakamura <hnakamur@gmail.com>
2024-09-04 21:17:08 +0900

    Support macos aarch64 in ch03/a-raw-syscall

--- ch03/a-raw-syscall/src/main.zig
+++ ch03/a-raw-syscall/src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 pub fn main() !void {
     const message = "Hello world from raw syscall!\n";
@@ -9,11 +10,36 @@ pub fn main() !void {
 fn writeToStdout(message: []const u8) usize {
     const msg_ptr = message.ptr;
     const len = message.len;
-    return asm volatile ("syscall"
-        : [ret] "=r" (-> usize),
-        : [number] "r" (@intFromEnum(std.os.linux.SYS.write)),
-          [arg1] "{rdi}" (1),
-          [arg2] "{rsi}" (msg_ptr),
-          [arg3] "{rdx}" (len),
-    );
+    switch (builtin.os.tag) {
+        .linux => {
+            switch (builtin.target.cpu.arch) {
+                .x86_64 => {
+                    return asm volatile ("syscall"
+                        : [ret] "=r" (-> usize),
+                        : [number] "r" (@intFromEnum(std.os.linux.SYS.write)),
+                          [arg1] "{rdi}" (1),
+                          [arg2] "{rsi}" (msg_ptr),
+                          [arg3] "{rdx}" (len),
+                    );
+                },
+                else => @panic("unsupported target cpu arch"),
+            }
+        },
+        .macos, .ios, .watchos, .tvos, .visionos => {
+            switch (builtin.target.cpu.arch) {
+                .aarch64 => {
+                    return asm volatile (
+                        \\ mov x16, 4
+                        \\ mov x0, 1
+                        \\ svc 0
+                        : [ret] "=r" (-> usize),
+                        : [arg2] "{x1}" (msg_ptr),
+                          [arg3] "{x2}" (len),
+                    );
+                },
+                else => @panic("unsupported target cpu arch"),
+            }
+        },
+        else => {},
+    }
 }
